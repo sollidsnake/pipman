6299c31aa5f5e5d6f6209939ed018ccf7f40a85e
n3f4s <tiv.crb@gmail.com>
2016-04-05 18:38:50 +0200

    automatic venv dir creation

--- src/pipman/venv_wrapper.py
+++ src/pipman/venv_wrapper.py
@@ -9,6 +9,8 @@ from misc import VENV_DIR, VENV_PIP
 
 import pip_wrapper as pip
 
+_venv_created = False
+
 def create_virtualenv():
     """Create virtualenv to install packages"""
 
@@ -19,13 +21,17 @@ def create_virtualenv():
         venv.create(VENV_DIR, with_pip=True)
         log.info('checking for pip upgrade') # upgrade pip
         pip.install('pip', '-U', in_venv=True)
+    global _venv_created
+    _venv_created = True
 
 def install_in_venv(package):
     """Install package in virtualenv"""
 
+    global _venv_created
+    if not _venv_created:
+        create_virtualenv()
     log = logging.getLogger("user")
     log.info("Installing '%s' in virutalenv", package)
 
     # install package in virtualenv pip
     pip.install(package, "--disable-pip-version-check", "--no-dependencies", in_venv=True)
-
