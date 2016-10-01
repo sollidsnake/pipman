8cc00c6892db23a440529af6b9cbfb7977cdde8e
Jesse Nazario <jessenzr@gmail.com>
2016-10-01 13:23:28 -0300

    color module in setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(name='pipman',
       license='GPL',
       keywords='archlinux pacman packaging',
       package_dir={'': 'src'},
-      packages=['pipman', 'pipman.pip2'],
+      packages=['pipman', 'pipman.pip2', 'pipman.color'],
 
       classifiers=[
 
