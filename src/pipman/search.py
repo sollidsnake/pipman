1db45ad7ee13455019354f1ab819a05ccaadb180
Jesse Nazario <jessenzr@gmail.com>
2016-10-19 00:48:13 -0200

    search with multiple terms

--- src/pipman/search.py
+++ src/pipman/search.py
@@ -3,6 +3,8 @@ from misc import ENCODING
 
 
 def search(packages: list):
-    out = subprocess.check_output(['pip', 'search', packages[0]])\
+    packages_join = " ".join(packages)
+    out = subprocess.check_output(['pip', 'search', packages_join])\
                     .decode(ENCODING)
+
     print(out)
